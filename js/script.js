c1dadcf4aff044278bc71cdddd45bc8c6c1957aa
Khant Phone <khantphone333@gmail.com>
2021-05-02 00:13:24 +0630

    footer completed

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*swiper start */
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 10,
+      grabCursor: true,
+      loop:true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+      },
+
+      loop:true,
+       autoplay: {
+        delay:1200,
+        disableOnInteraction: false,
+
+      },
+    });
+/*swiper end */
+/*aos start*/
+ AOS.init({	    	
+	    	duration:1400,
+	    	delay:250,
+	    	offset:100	    	  		    	
+	    });
+/*aos end*/
